6313cdff0ae8f5d47f9121d80f8f883a539e9b8d
wngu17ko <166520677+wngu17ko@users.noreply.github.com>
2024-06-12 09:44:37 -0400

    q2_tachesomm3.rb

--- tasks/q2_tachesomm3.rb
+++ tasks/q2_tachesomm3.rb
@@ -4,5 +4,5 @@ valeur = gets.chomp.to_i
 compte_sup = notes.count { |num| num = valeur}  
 compte_inf = notes.count   { |num| num = valeur} 
 compte_egale = notes.count { |num| num = valeur} 
-puts "Il y a #{compte_sup} nombres superior que #{valeur}, #{compte_inf} nombres inferior que #{valeur},
+puts "Il y a #{compte_sup} nombres superieur que #{valeur}, #{compte_inf} nombres inferior que #{valeur},
  et #{compte_egale} nombres qui est egale a #{valeur}."   
